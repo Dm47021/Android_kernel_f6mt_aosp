dd4e3729d3c15b82a120332e37c5432295b9649b
dm47021 <dm47021@hotmail.com>
2014-11-14 01:57:46 -0500

    Revert "The Reaper Cpu gov is born.."
    This reverts commit 7eec24c3cb604bc817994984ba93be7efb02dad7.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -375,9 +375,6 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_REAPER)
-extern struct cpufreq_governor cpufreq_gov_reaper;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_reaper)
 #endif
 
 
